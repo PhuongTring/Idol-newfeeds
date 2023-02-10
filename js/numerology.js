c257abed420a402ef8133906cebd56074b59234f
PhuongTring <phuongtrinh.2146@gmail.com>
2023-02-11 00:44:01 +0700

    add filtering logic for renderItem

--- js/numerology.js
+++ js/numerology.js
@@ -1,35 +1,38 @@
+let inputDate = document.getElementById("userDate");
+let inputMonth = document.getElementById("userMonth");
+let inputYear = document.getElementById("userYear");
+
+let render = document.getElementById("render");
+let renderItem = null;
+
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
+  let userDate = inputDate.value;
+  let userMonth = inputMonth.value;
+  let userYear = inputYear.value;
 
-    let userDate = document.getElementById("userDate").value;
-    let userMonth = document.getElementById("userMonth").value;
-    if (userMonth > 12){
-        return alert("Nhap sai ruiii");
-    }
-    let userYear = document.getElementById("userYear").value;
-    let chuoi = userDate + userMonth + userYear;
+  let chuoi = userDate + userMonth + userYear;
 
-    let render = document.getElementById("render");
-
-    let stringArray = chuoi.split("");
-    let sum=0;
-    for (let i=0; i<stringArray.length; i++){
-        sum += parseInt(stringArray[i]);
-    }
-    if (sum==11){
-        sum=11;
-    } else if (sum==22){
-        sum=22;
-    } else if (sum==33){
-        sum=33;
-    } else{
-        let tempDonvi=sum%10;
-        let temChuc=parseInt(sum/10);
-        sum=tempDonvi+temChuc;
-    }
-
-    let divId = "number"+sum;
-    let renderItem=document.getElementById(divId);
-    renderItem.classList.add("display-item");
-    render.innerHTML = `Your date is ${userDate} your month is ${userMonth} your year is ${userYear}`;
+  let stringArray = chuoi.split("");
+  let sum = 0;
+  for (let i = 0; i < stringArray.length; i++) {
+    sum += parseInt(stringArray[i]);
+  }
+  if (sum == 11) {
+    sum = 11;
+  } else if (sum == 22) {
+    sum = 22;
+  } else if (sum == 33) {
+    sum = 33;
+  } else {
+    let tempDonvi = sum % 10;
+    let temChuc = parseInt(sum / 10);
+    sum = tempDonvi + temChuc;
+  }
+  if (renderItem) {
+    renderItem.classList.remove("display-item");
+  }
+  renderItem = document.getElementById(`number${sum}`);
+  renderItem.classList.add("display-item");
+  render.innerHTML = `Your date is ${userDate} your month is ${userMonth} your year is ${userYear}`;
 }
